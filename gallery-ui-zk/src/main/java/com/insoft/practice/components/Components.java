4b11c438e0d43828d02fdfca72a81f6dd888aa36
mindaugas.arsulis <mindaugas.arsulis@insoft.lt>
2019-03-12 17:08:19 +0200

    add ZK Component

--- gallery-ui-zk/src/main/java/com/insoft/practice/components/Components.java
+++ gallery-ui-zk/src/main/java/com/insoft/practice/components/Components.java
@@ -1,15 +1,14 @@
 package com.insoft.practice.components;
 
 import com.insoft.practice.bl.services.ImageService;
+import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.bind.annotation.Init;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.HtmlMacroComponent;
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.event.Events;
-import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Button;
-
 
 public class Components extends HtmlMacroComponent {
     private static final long serialVersionUID = 7935967626121317003L;
@@ -20,22 +19,34 @@ public class Components extends HtmlMacroComponent {
     @Wire
     private Button delete_button;
 
-    private String someWord;
+    private Long id;
 
     public Components () {
-//        Executions.createComponents("/zul/component/component.zul", this, null);
-        setMacroURI("/zul/component/component.zul");
-        compose();
+
     }
 
+    @Init
+    public void init(@ExecutionArgParam("label") Long id) {
+        this.id = id;
+    }
+
+
+
+    @Listen("onClick=#label")
     public void doDeleteImage() {
-        Events.postEvent(new Event("onClick", this));
-        smartUpdate("value", someWord);
+        imageService.deleteImage(id);
+        Executions.getCurrent().sendRedirect("/zul/delete.zul?id=" + id);
     }
 
-    protected void renderProperties(org.zkoss.zk.ui.sys.ContentRenderer renderer)
-            throws java.io.IOException {
-        super.renderProperties(renderer);
-        render(renderer, "value", someWord);
-    }
+//    protected void renderProperties(org.zkoss.zk.ui.sys.ContentRenderer renderer)
+//            throws java.io.IOException {
+//        super.renderProperties(renderer);
+//        render(renderer, "value", someWord);
+    //        Events.postEvent(new Event("onClick", this));
+//        smartUpdate("value", someWord);
+//    }
 }
+
+//        Events.postEvent(new Event("onClick", this, null));
+//        Executions.createComponents("/WEB-INF/component/component.zul", this, null);
+//        compose();
